e33c5a64b27fbb8fe7d342b99e95264f637ee62d
Thomas Gleixner <tglx@linutronix.de>
2009-12-09 09:32:03 +0100

    sched: Protect sched_rr_get_param() access to task->sched_class
    commit dba091b9e3522b9d32fc9975e48d3b69633b45f0 upstream
    
    sched_rr_get_param calls
    task->sched_class->get_rr_interval(task) without protection
    against a concurrent sched_setscheduler() call which modifies
    task->sched_class.
    
    Serialize the access with task_rq_lock(task) and hand the rq
    pointer into get_rr_interval() as it's needed at least in the
    sched_fair implementation.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <alpine.LFD.2.00.0912090930120.3089@localhost.localdomain>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- kernel/sched.c
+++ kernel/sched.c
@@ -6996,6 +6996,8 @@ SYSCALL_DEFINE2(sched_rr_get_interval, pid_t, pid,
 {
 	struct task_struct *p;
 	unsigned int time_slice;
+	unsigned long flags;
+	struct rq *rq;
 	int retval;
 	struct timespec t;
 
@@ -7012,7 +7014,9 @@ SYSCALL_DEFINE2(sched_rr_get_interval, pid_t, pid,
 	if (retval)
 		goto out_unlock;
 
-	time_slice = p->sched_class->get_rr_interval(p);
+	rq = task_rq_lock(p, &flags);
+	time_slice = p->sched_class->get_rr_interval(rq, p);
+	task_rq_unlock(rq, &flags);
 
 	rcu_read_unlock();
 	jiffies_to_timespec(time_slice, &t);
